de3346dc5df4479c6c39dcf9a6dc36121cee5500
Dwayne Robinson <fdwr@hotmail.com>
2025-02-03 23:20:54 -0800

    Update PushGitHub to avoid taking so long

--- PushGitHub.bat
+++ PushGitHub.bat
@@ -1,11 +1,22 @@
 @echo off
 setlocal
 
-:: Move up a directory. Otherwise git complains:
+:: Move up a directory. Otherwise git complains with:
 :: "You need to run this command from the toplevel of the working tree."
 pushd %~dp0\..
 
-set command=git subtree push --prefix BitString https://github.com/fdwr/BitString.git master
+:: Normally we'd use subtree push, but that takes longer and longer the more commits there are in the repo,
+:: as git rereads the whole history to split out any changes under that directory. By instead splitting the
+:: repo and then pushing that branch (and keeping the branch around), it takes barely longer than a normal
+:: push command.
+
+set command=git subtree split --rejoin --prefix BitString -b BitString
+echo %command%
+%command%
+
+::Avoid subtree push, instead pushing from the split branch.
+:: set command=git subtree push --prefix BitString https://github.com/fdwr/BitString.git master
+set command=git push https://github.com/fdwr/BitString.git BitString:master
 echo %command%
 %command%
 
