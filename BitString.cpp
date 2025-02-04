c72f21ee545160406fc8c5618a2fb4b01ad0b60f
Dwayne Robinson <fdwr@hotmail.com>
2025-02-03 19:58:36 -0800

    BitString add struct field writing, read/write of unaligned float, clean printing code

--- BitString.cpp
+++ BitString.cpp
@@ -1,30 +1,40 @@
 ﻿// Prints:
 //
-// Test against generated data:
-// LE data 19-bit: 0,7FFFF,0,7FFFF,0,7FFFF,0,7FFFF,
-//       as bytes: 00,00,F8,FF,3F,00,00,FE,FF,0F,00,80,FF,FF,03,00,E0,FF,FF,
-// BE data 19-bit: 0,7FFFF,0,7FFFF,0,7FFFF,0,7FFFF,
-//       as bytes: 00,00,1F,FF,FC,00,00,7F,FF,F0,00,01,FF,FF,C0,00,07,FF,FF,
-// 
-// Test against known constant data:
-// LE data 12-bit: 321,654,987,CBA,
-//       as bytes: 21,43,65,87,A9,CB,00,00,00,
-// BE data 12-bit: 321,654,987,CBA,
-//       as bytes: 32,16,54,98,7C,BA,00,00,00,
-// 
-// Test against written data:
-// LE data 13-bit: 0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,
-//       as bytes: 00,20,00,08,80,01,40,00,0A,80,01,38,00,08,20,01,28,80,05,C0,00,1A,80,03,78,00,
-// BE data 13-bit: 0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,
-//       as bytes: 00,00,00,40,04,00,30,02,00,14,00,C0,07,00,40,02,40,14,00,B0,06,00,34,01,C0,0F,
+//  Test array reading of generated bitmasks:
+//      LE 19-bit data @0: 0,7FFFF,0,7FFFF,0,7FFFF,0,7FFFF
+//               as bytes: 00,00,F8,FF,3F,00,00,FE,FF,0F,00,80,FF,FF,03,00,E0,FF,FF
+//      BE 19-bit data @0: 0,7FFFF,0,7FFFF,0,7FFFF,0,7FFFF
+//               as bytes: 00,00,1F,FF,FC,00,00,7F,FF,F0,00,01,FF,FF,C0,00,07,FF,FF
 //
-// Test with struct:
-// a=321, b=7FFF, c=6,
+//  Test array reading of known constant data:
+//      LE 12-bit data @0: 321,654,987,CBA
+//               as bytes: 21,43,65,87,A9,CB
+//      BE 12-bit data @0: 321,654,987,CBA
+//               as bytes: 32,16,54,98,7C,BA
+//
+//  Test writing/reading array of increasing sequence:
+//      LE 13-bit data @0: 0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F
+//               as bytes: 00,20,00,08,80,01,40,00,0A,80,01,38,00,08,20,01,28,80,05,C0,00,1A,80,03,78,00
+//      BE 13-bit data @0: 0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F
+//               as bytes: 00,00,00,40,04,00,30,02,00,14,00,C0,07,00,40,02,40,14,00,B0,06,00,34,01,C0,0F
+//
+//  Test reading/writing fields inside struct:
+//      Read NE data: a=321, b=7FFF, c=6
+//           as bytes: 21,E3,FF,6F
+//      Write NE data: a=321, b=7FFF, c=6
+//           as bytes: 21,E3,FF,6F
+//
+//  Test reading/writing float32 at unaligned offset:
+//      32-bit LE data @5: 3.141593
+//               as bytes: 60,FB,21,09,08
+//      32-bit BE data @5: 3.141593
+//               as bytes: 02,02,48,7E,D8
 
+// Needs C++20.
 #include <stdint.h>
 #include <limits.h>
 #include <stdio.h>
-#include <bit> // C++23 std::byteswap
+#include <bit> // std::endian
 #include <span>
 #include <new> // std::launder
 #include <assert.h>
@@ -196,19 +206,49 @@ void WriteBitString(
 
 void PrintElementsOfGivenBitSize(
     std::span<uint8_t const> data,
+    size_t bitOffset,
+    size_t bitSize,
     size_t elementCount,
-    size_t elementBitSize,
     std::endian endianness
 )
 {
     for (size_t i = 0; i < elementCount; ++i)
     {
-        size_t elementBitOffset = i * elementBitSize;
-        uint32_t elementValue = ReadBitString(data, elementBitOffset, elementBitSize, endianness);
-        printf("%X,", elementValue);
+        size_t elementBitOffset = bitOffset + i * bitSize;
+        uint32_t elementValue = ReadBitString(data, elementBitOffset, bitSize, endianness);
+        printf((i == 0) ? "%X" : ",%X", elementValue);
     }
 }
 
+void PrintBytes(std::span<uint8_t const> data)
+{
+    for (size_t i = 0, size = data.size(); i < size; ++i)
+    {
+        printf((i == 0) ? "%02X" : ",%02X", data[i]);
+    }
+}
+
+void PrintLeAndBeValues(
+    std::span<uint8_t const> dataLe,
+    std::span<uint8_t const> dataBe,
+    size_t bitOffset,
+    size_t bitSize,
+    size_t elementCount
+)
+{
+    assert(dataLe.size() == dataBe.size());
+
+    printf("    LE %zu-bit data @%zu: ", bitSize, bitOffset);
+    PrintElementsOfGivenBitSize(dataLe, bitOffset, bitSize, elementCount, std::endian::little); printf("\n");
+    printf("             as bytes: "); PrintBytes(dataLe); printf("\n");
+
+    printf("    BE %zu-bit data @%zu: ", bitSize, bitOffset);
+    PrintElementsOfGivenBitSize(dataBe, bitOffset, bitSize, elementCount, std::endian::big); printf("\n");
+    printf("             as bytes: "); PrintBytes(dataBe); printf("\n");
+}
+
+
+#if 0
 void OldPrintElementsOfGivenBitSize(
     std::span<uint8_t const> data,
     size_t elementCount,
@@ -244,15 +284,9 @@ void OldPrintElementsOfGivenBitSize(
         printf("%X,", elementValue);
     }
 }
+#endif
 
-void PrintBytes(std::span<uint8_t const> data)
-{
-    for (auto d : data)
-    {
-        printf("%02X,", d);
-    }
-}
-
+// Basically like the x86 bts instruction, except it can invert indices in bytes for BE.
 void SetBit(
     std::span<uint8_t> data, // Size limited to 500GB's on 32-bit systems.
     size_t bitOffset,
@@ -265,9 +299,15 @@ void SetBit(
     data[bitOffset / CHAR_BIT] |= 1 << (bitOffset & byteMask);
 }
 
+template <typename T>
+auto wrapStructAsBytes(T& t) -> std::span<uint8_t>
+{
+    return {reinterpret_cast<uint8_t*>(&t), sizeof(t)};
+};
+
 int main()
 {
-    printf("Test against generated data:\n");
+    printf("Test array reading of generated bitmasks:\n");
     {
         std::uint8_t elementsLe[19] = {};
         std::uint8_t elementsBe[19] = {};
@@ -285,40 +325,22 @@ int main()
             }
         }
 
-        printf("LE data %zu-bit: ", elementBitSize);
-        //OldPrintElementsOfGivenBitSize(elementsLe, elementCount, elementBitSize, std::endian::little); printf("\n");
-        PrintElementsOfGivenBitSize(elementsLe, elementCount, elementBitSize, std::endian::little); printf("\n");
-        printf("      as bytes: "); PrintBytes(elementsLe); printf("\n");
-
-        printf("BE data %zu-bit: ", elementBitSize);
-        //OldPrintElementsOfGivenBitSize(elementsBe, elementCount, elementBitSize, std::endian::big); printf("\n");
-        PrintElementsOfGivenBitSize(elementsBe, elementCount, elementBitSize, std::endian::big); printf("\n");
-        printf("      as bytes: "); PrintBytes(elementsBe); printf("\n");
+        PrintLeAndBeValues(elementsLe, elementsBe, 0, elementBitSize, elementCount);
     }
     printf("\n");
 
-    printf("Test against known constant data:\n");
+    printf("Test array reading of known constant data:\n");
     {
+        const std::uint8_t elementsLe[] = {0x21, 0x43, 0x65, 0x87, 0xA9, 0xCB};
+        const std::uint8_t elementsBe[] = {0x32, 0x16, 0x54, 0x98, 0x7C, 0xBA};
         constexpr size_t elementBitSize = 12;
-        constexpr size_t elementCount = 4;
-        constexpr size_t byteCount = (elementCount * elementBitSize + CHAR_BIT - 1) / CHAR_BIT;
+        constexpr size_t elementCount = sizeof(elementsLe) * CHAR_BIT / elementBitSize;
 
-        const std::uint8_t elementsLe[byteCount + 3] = {0x21, 0x43, 0x65, 0x87, 0xA9, 0xCB};
-        const std::uint8_t elementsBe[byteCount + 3] = {0x32, 0x16, 0x54, 0x98, 0x7C, 0xBA};
-
-        printf("LE data %zu-bit: ", elementBitSize);
-        //OldPrintElementsOfGivenBitSize(elementsLe, elementCount, elementBitSize, std::endian::little); printf("\n");
-        PrintElementsOfGivenBitSize(elementsLe, elementCount, elementBitSize, std::endian::little); printf("\n");
-        printf("      as bytes: "); PrintBytes(elementsLe); printf("\n");
-
-        printf("BE data %zu-bit: ", elementBitSize);
-        //OldPrintElementsOfGivenBitSize(elementsBe, elementCount, elementBitSize, std::endian::big); printf("\n");
-        PrintElementsOfGivenBitSize(elementsBe, elementCount, elementBitSize, std::endian::big); printf("\n");
-        printf("      as bytes: "); PrintBytes(elementsBe); printf("\n");
+        PrintLeAndBeValues(elementsLe, elementsBe, 0, elementBitSize, elementCount);
     }
     printf("\n");
 
-    printf("Test against written data:\n");
+    printf("Test writing/reading array of increasing sequence:\n");
     {
         std::uint8_t elementsLe[26] = {};
         std::uint8_t elementsBe[26] = {};
@@ -333,20 +355,12 @@ int main()
             WriteBitString(/*inout*/ elementsBe, bitOffset, elementBitSize, std::endian::big,    value);
         }
 
-        printf("LE data %zu-bit: ", elementBitSize);
-        //OldPrintElementsOfGivenBitSize(elementsLe, elementCount, elementBitSize, std::endian::little); printf("\n");
-        PrintElementsOfGivenBitSize(elementsLe, elementCount, elementBitSize, std::endian::little); printf("\n");
-        printf("      as bytes: "); PrintBytes(elementsLe); printf("\n");
-
-        printf("BE data %zu-bit: ", elementBitSize);
-        //OldPrintElementsOfGivenBitSize(elementsBe, elementCount, elementBitSize, std::endian::big); printf("\n");
-        PrintElementsOfGivenBitSize(elementsBe, elementCount, elementBitSize, std::endian::big); printf("\n");
-        printf("      as bytes: "); PrintBytes(elementsBe); printf("\n");
+        PrintLeAndBeValues(elementsLe, elementsBe, 0, elementBitSize, elementCount);
     }
     printf("\n");
 
     // Note I haven't actually tested this case on a BE machine (I don't have any :b), but it *should* work. 🤞
-    printf("Test with struct:\n");
+    printf("Test reading/writing fields inside struct:\n");
     {
         struct TestStruct
         {
@@ -372,16 +386,43 @@ int main()
             .b = 0x7FFF,
             .c = 0x6
         };
-        auto wrapAsBytes = [](TestStruct& ts) -> std::span<uint8_t>
-        {
-            return { reinterpret_cast<uint8_t*>(&ts), sizeof(ts) };
-        };
 
-        uint32_t aValue = ReadBitString(wrapAsBytes(testStruct), 0,     13, std::endian::native);
-        uint32_t bValue = ReadBitString(wrapAsBytes(testStruct), 13,    15, std::endian::native);
-        uint32_t cValue = ReadBitString(wrapAsBytes(testStruct), 13+15, 3,  std::endian::native);
+        uint32_t aValue = ReadBitString(wrapStructAsBytes(testStruct), 0,     13, std::endian::native);
+        uint32_t bValue = ReadBitString(wrapStructAsBytes(testStruct), 13,    15, std::endian::native);
+        uint32_t cValue = ReadBitString(wrapStructAsBytes(testStruct), 13+15, 3,  std::endian::native);
 
-        printf("a=%X, b=%X, c=%X,", aValue, bValue, cValue);
+        printf("    Read NE data: a=%X, b=%X, c=%X\n", aValue, bValue, cValue);
+        printf("         as bytes: "); PrintBytes(wrapStructAsBytes(testStruct)); printf("\n");
+
+        testStruct = {};
+        WriteBitString(wrapStructAsBytes(testStruct), 0,     13, std::endian::native, 0x321);
+        WriteBitString(wrapStructAsBytes(testStruct), 13,    15, std::endian::native, 0x7FFF);
+        WriteBitString(wrapStructAsBytes(testStruct), 13+15, 3,  std::endian::native, 0x6);
+
+        printf("    Write NE data: a=%X, b=%X, c=%X\n", testStruct.a, testStruct.b, testStruct.c);
+        printf("         as bytes: "); PrintBytes(wrapStructAsBytes(testStruct)); printf("\n");
+    }
+    printf("\n");
+
+    printf("Test reading/writing float32 at unaligned offset:\n");
+    {
+        uint8_t bufferLe[sizeof(float) + 1] = {};
+        uint8_t bufferBe[sizeof(float) + 1] = {};
+        constexpr size_t bitOffset = 5;
+        constexpr size_t bitSize = sizeof(float) * CHAR_BIT;
+        constexpr uint32_t piValue = std::bit_cast<uint32_t>(3.14159265358979323846f);
+        float readbackValue;
+
+        WriteBitString(bufferLe, bitOffset, bitSize, std::endian::little, piValue);
+        WriteBitString(bufferBe, bitOffset, bitSize, std::endian::big,    piValue);
+
+        readbackValue = std::bit_cast<float>(ReadBitString(bufferLe, bitOffset, bitSize, std::endian::little));
+        printf("    %zu-bit LE data @%zu: %f\n", bitSize, bitOffset, readbackValue);
+        printf("             as bytes: "); PrintBytes(bufferLe); printf("\n");
+
+        readbackValue = std::bit_cast<float>(ReadBitString(bufferBe, bitOffset, bitSize, std::endian::big));
+        printf("    %zu-bit BE data @%zu: %f\n", bitSize, bitOffset, readbackValue);
+        printf("             as bytes: "); PrintBytes(bufferBe); printf("\n");
     }
     printf("\n");
 }
